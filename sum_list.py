40e4755c158d76374a62f9005f4bb71ee227ba27
mwangi-stacy <njoki.mwangi@strathmore.edu>
2025-04-26 02:30:16 +0300

    The first attempt at task one of the assignment

--- sum_list.py
+++ sum_list.py
@@ -1,1 +1,24 @@
+#Allow the user to enter a list of numbers, with no specified limit
+numbers =input("Enter numbers with spaces in between:")
+
+
+#Creating the function to sum up all those numbers
+def sum_elements():
+    #To split the numbers into individual strings
+    numbers_list = numbers.split(" ")
+
+    #To convert the split numbers into integers for addition
+    number = [int(num) for num in numbers_list]
+
+    #To add the elements together
+    total_sum = sum(number)
+
+    #To print out the result
+    print("The total sum for the numbers you gave is:", total_sum)
+
+# Call the function
+sum_elements()
+
+
+
 
